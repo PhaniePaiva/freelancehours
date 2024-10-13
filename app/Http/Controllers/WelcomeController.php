632b52113a2e2cc0ed32816107899972925f59b2
Stephanie Paiva <stephaniepaiva2001@gmail.com>
2024-10-13 15:15:41 -0300

    Fundamentos parte 2

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+// use App\Models\User;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
@@ -11,6 +12,26 @@ class WelcomeController extends Controller
      */
     public function __invoke(Request $request)
     {
+
+        // $user = User::find(1);
+
+        // $user = User::query()->create([
+        //     'name'     => 'Stephanie Paiva',
+        //     'email'    => 'stephanie@gmail.com',
+        //     'password' => 'jeremias',
+
+        // ]);
+        
+        // $user->email_verified_at = now();
+
+        // $user->save();
+
+        // $user->update(['email_verified_at' => now()]);
+
+        // $user->email_verified_at = now();
+        // $user->save();
+        // $user->update(['email_verified_at' => now()->subMonths(10)]);
+        // dd($user->email_verified_at->diffForHumans());
 
         // return WelcomeController::class;
         return view('welcome');
--- resources/views/livewire/teste.blade.php
+++ resources/views/livewire/teste.blade.php
@@ -1,11 +1,11 @@
 <div>
     <h1>Meu componente Livewire</h1>
-    <input wire:model.live="search">
+    <input wire:model.live="search" />
 
     <br>
 
     <ul>
-        @foreach($users as @user)
+        @foreach($users as $user)
         <li>{{ $user->name }}</li>
         @endforeach
     </ul>
